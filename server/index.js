f4bb7c547604995118834a9633ece57b2cd0d3c8
Shantanu Mahale <46351765+shantanubr@users.noreply.github.com>
2023-12-31 19:35:53 +0530

    create todo and update todo apis

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { createTodo, updateTodo } = require('./types');
 
 const app = express();
 const PORT = 3001;
@@ -10,11 +11,27 @@ app.get('/todos', (req, res) => {
 });
 
 app.post('/create-todo', (req, res) => {
-
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        });
+        return;
+    }
+    // put data in mongodb
 });
 
 app.put('/update-todo', (req, res) => {
-
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        });
+        return;
+    }
+    // put data in mongodb
 });
 
 app.listen(PORT, () => {
