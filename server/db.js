b0cd98adf5ce4c1c247231b6bd92992187314507
Shantanu Mahale <46351765+shantanubr@users.noreply.github.com>
2024-01-01 11:59:10 +0530

    gitignore config

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/todos");
+mongoose.connect("mongodb+srv://mahaleshantanu4:1234@cluster0.n0nkqgt.mongodb.net/");
 
 const todoSchema = mongoose.Schema({
     title: String,
